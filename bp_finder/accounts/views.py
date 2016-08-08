3f7e00a1ca6097b4aa9ee72ecc075ba34e3ce248
Patrick O'Laughlin <polaughlin27@gmail.com>
2016-08-08 10:55:54 -0500

    Removed forms from views.py

--- bp_finder/accounts/views.py
+++ bp_finder/accounts/views.py
@@ -26,9 +26,6 @@ from .serializers import (
 )
 
 
-from .forms import UserLoginForm, UserRegisterForm
-
-
 class CreateUserView(CreateAPIView):
 
     model = get_user_model()
@@ -36,48 +33,6 @@ class CreateUserView(CreateAPIView):
         AllowAny
     ]
     serializer_class = UserSerializer
-
-
-def login_view(request):
-    print(request.user.is_authenticated())
-    next = request.GET.get('next')
-    title = "Login"
-    form = UserLoginForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        if next:
-            return redirect(next)
-        return redirect("/")
-    return render(request, "form.html", {"form": form, "title": title})
-
-
-def register_view(request):
-    print(request.user.is_authenticated())
-    next = request.GET.get('next')
-    title = "Register"
-    form = UserRegisterForm(request.POST or None)
-    if request.user.is_authenticated:
-        return redirect("/")
-
-    if form.is_valid():
-        user = form.save(commit=False)
-        password = form.cleaned_data.get('password')
-        user.set_password(password)
-        user.save()
-        new_user = authenticate(username=user.username, password=password)
-        login(request, new_user)
-        if next:
-            return redirect(next)
-        return redirect("/")
-
-    context = {
-        "form": form,
-        "title": title
-    }
-    return render(request, "form.html", context)
 
 
 def logout_view(request):
